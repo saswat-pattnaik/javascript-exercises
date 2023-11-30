a3527709b84ba3fa601a1a64e3d2df9b1b248750
saswat-pattnaik <148523561+saswat-pattnaik@users.noreply.github.com>
2023-11-30 06:11:59 +0530

    fix: complete required function for reversing string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let ans = "";
 
+    for(let i = str.length - 1 ; i >= 0 ; i--){
+        ans = ans + str[i];
+    }
+
+    return ans;
 };
 
 // Do not edit below this line
