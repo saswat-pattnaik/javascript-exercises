ad05cd226449907e74d17b1f05930be6016aacc7
saswat-pattnaik <148523561+saswat-pattnaik@users.noreply.github.com>
2023-11-30 08:01:46 +0530

    fix: add required code for 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    const ans = [...arr];
+    for(let i=0;i<toRemove.length;i++){
+        const elem = toRemove[i];
+        let index = ans.indexOf(elem);
+        if(index > -1){
+            ans.splice(index, 1);
+        }
+    }
+    return ans;
 };
 
 // Do not edit below this line
