943779fb4e6e4d52139bdecb42e435eabeda72fa
saswat-pattnaik <148523561+saswat-pattnaik@users.noreply.github.com>
2023-12-04 18:43:19 +0530

    fix: solve 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let toTest = "";
 
+    for(let i=0; i < str.length; i++){
+        const char = str.charCodeAt(i);
+        if((char >= 65 && char <= 90) || (char >= 97 && char <= 122) || (char >= 48 && char <= 57)){
+            toTest += str[i].toLowerCase();
+        }
+    }
+
+    let i=0, j=toTest.length-1;
+
+    while(i < j){
+        if(toTest[i] != toTest[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
