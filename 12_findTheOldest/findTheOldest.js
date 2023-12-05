7e717d7ccff5c0d20e8b7d3b407338bde576cc8a
saswat-pattnaik <148523561+saswat-pattnaik@users.noreply.github.com>
2023-12-05 08:04:23 +0530

    fix: solve last exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let maxAge = 0;
+    let ans;
+    for(let i=0; i<people.length; i++){
+        const {name, yearOfBirth, yearOfDeath} = people[i];
 
+        let age = 0;
+
+        if(!yearOfDeath){
+            const dateNow = new Date();
+
+            age = dateNow.getFullYear() - yearOfBirth;
+        }else{
+            age = yearOfDeath - yearOfBirth;
+        }
+        if(age > maxAge){
+            ans = people[i];
+            maxAge = age;
+        }
+
+    }
+
+    return ans;
 };
 
 // Do not edit below this line
